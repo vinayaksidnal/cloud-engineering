a8b4ad1f9bc583030140a70f11bd283e8ed6d115
Shidnal <VinayakS.Shidnal@bp.com>
2021-12-27 20:46:17 +0530

    updated function app always on is not allowed

--- 3_coding/Reviews/__init__.py
+++ 3_coding/Reviews/__init__.py
@@ -2,50 +2,49 @@ import logging
 
 import azure.functions as func
 import json
-from azure.cosmos import cosmos_client
+import os
+from azure.cosmos import CosmosClient
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    cosmos_config = {
-    "ENDPOINT": "ENDPOINT_FROM_YOUR_COSMOS_ACCOUNT",
-    "PRIMARYKEY": "KEY_FROM_YOUR_COSMOS_ACCOUNT",
-    "DATABASE": "DATABASE_ID",  
-    "CONTAINER": "YOUR_CONTAINER_ID" 
-    }
+    ENDPOINT =  os.environ['endpoint']
+    PRIMARYKEY = os.environ['key'] 
+    DATABASE =  os.environ['database']  
+    CONTAINER =  os.environ['container'] 
 
-    CONTAINER_LINK = f"dbs/{cosmos_config['DATABASE']}/colls/{cosmos_config['CONTAINER']}"
-    FEEDOPTIONS = {}
-    FEEDOPTIONS["enableCrossPartitionQuery"] = True
+    source = req.params.get('source')
 
-    query = {
-        "query": f"SELECT * from c"
-    }
+    client = CosmosClient(ENDPOINT, credential=PRIMARYKEY)
+    database = client.get_database_client(DATABASE)
+    container = database.get_container_client(CONTAINER)
 
+    if source:
+        
+        query = 'SELECT * FROM reviews p WHERE p.source = "' + source + '"'
 
-    client = cosmos_client.CosmosClient(
-        url_connection=cosmos_config["ENDPOINT"], auth={"masterKey": cosmos_config["PRIMARYKEY"]}
-    )
+        results = container.query_items(
+            query=query
+        )
+        resList = list(results)
 
-    results = client.QueryItems(CONTAINER_LINK, query, FEEDOPTIONS)
-
-    print(list(results))
-
+        # respo = json.dumps(list(resList), indent=4)
+        
+        # print(comment)
+        
+        # name = req.params.get('name')
+        # if not name:
+        #     try:
+        #         req_body = req.get_json()
+        #     except ValueError:
+        #         pass
+        #     else:
+        #         name = req_body.get('name')
+        return func.HttpResponse(resList[0]['comment'])
     
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a source value as either galaxy or mars in the query string or in the request body for a personalized response.",
              status_code=200
         )
