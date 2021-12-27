b8883e7313de0222dff5e236acd407e0bfe76e90
Shidnal <VinayakS.Shidnal@bp.com>
2021-12-27 18:30:41 +0530

    updated function app always on is not allowed

--- 3_coding/Reviews/__init__.py
+++ 3_coding/Reviews/__init__.py
@@ -1,11 +1,38 @@
 import logging
 
 import azure.functions as func
+import json
+from azure.cosmos import cosmos_client
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    cosmos_config = {
+    "ENDPOINT": "ENDPOINT_FROM_YOUR_COSMOS_ACCOUNT",
+    "PRIMARYKEY": "KEY_FROM_YOUR_COSMOS_ACCOUNT",
+    "DATABASE": "DATABASE_ID",  
+    "CONTAINER": "YOUR_CONTAINER_ID" 
+    }
+
+    CONTAINER_LINK = f"dbs/{cosmos_config['DATABASE']}/colls/{cosmos_config['CONTAINER']}"
+    FEEDOPTIONS = {}
+    FEEDOPTIONS["enableCrossPartitionQuery"] = True
+
+    query = {
+        "query": f"SELECT * from c"
+    }
+
+
+    client = cosmos_client.CosmosClient(
+        url_connection=cosmos_config["ENDPOINT"], auth={"masterKey": cosmos_config["PRIMARYKEY"]}
+    )
+
+    results = client.QueryItems(CONTAINER_LINK, query, FEEDOPTIONS)
+
+    print(list(results))
+
+    
     name = req.params.get('name')
     if not name:
         try:
